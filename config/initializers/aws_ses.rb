b1ff3ce2fcf8fb42f0422ffe0da8363924d7a770
Sergii Chub <s.chub@exlabs.pl>
2019-11-14 15:11:23 +0200

    [CAZ-1477] Migrate from govuk-lint (#37)

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# :nocov:
 # SES_ACCESS_KEY_ID may override AWS_ACCESS_KEY_ID
 access_id = ENV.fetch('SES_ACCESS_KEY_ID') do
   ENV.fetch('AWS_ACCESS_KEY_ID', 'example_key')
@@ -10,7 +9,6 @@ end
 secret_key = ENV.fetch('SES_SECRET_ACCESS_KEY') do
   ENV.fetch('AWS_SECRET_ACCESS_KEY', 'example_key')
 end
-# :nocov:
 
 creds = Aws::Credentials.new(access_id, secret_key)
 
