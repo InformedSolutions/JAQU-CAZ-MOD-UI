c4489e5242a61127a52ed792355bfa461f0ac24c
Sergii Chub <s.chub@exlabs.pl>
2020-03-24 11:12:39 +0100

    [CAZB-892] Removal of row count from file naming convention (#57)

--- spec/requests/upload_spec.rb
+++ spec/requests/upload_spec.rb
@@ -6,7 +6,7 @@ describe UploadController, type: :request do
   let(:file_path) do
     File.join(
       'spec',
-      'fixtures', 'files', 'csv', 'CAZ-2020-01-08-5.csv'
+      'fixtures', 'files', 'csv', 'CAZ-2020-01-08.csv'
     )
   end
 
@@ -87,7 +87,7 @@ describe UploadController, type: :request do
 
     context 'with invalid params' do
       let(:file_path) do
-        File.join('spec', 'fixtures', 'files', 'csv', 'empty', 'CAZ-2020-01-08.csv')
+        File.join('spec', 'fixtures', 'files', 'csv', 'empty', 'CAZ-2020-01-08-.csv')
       end
 
       it 'returns error' do
