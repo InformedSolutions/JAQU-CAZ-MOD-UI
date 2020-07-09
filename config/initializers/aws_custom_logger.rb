084ed045e0dcda1ecb3b441f2b14cb0d4d14e989
Piotr Kałamarski <pkalamarski@gmail.com>
2020-07-09 15:23:13 +0200

    [CAZB-2092] Fix "confirmation_code" leak in logs (#158)

--- config/initializers/aws_custom_logger.rb
+++ config/initializers/aws_custom_logger.rb
@@ -10,7 +10,7 @@ module Aws
         # Do not log AWS::SES response by the reason that we should not see user email
         # TO DO: Modify response to log it without user email
         def log(config, response)
-          return if config.sigv4_signer.service == 'ses'
+          return if %w[ses cognito-idp].include?(config.sigv4_signer.service)
 
           config.logger.send(config.log_level, format(config, response))
         end
